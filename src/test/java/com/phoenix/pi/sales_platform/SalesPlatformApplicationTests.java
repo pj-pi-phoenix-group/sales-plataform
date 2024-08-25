80f8f864e8a51ce22993c464825330beb4007c5b
MattFC <matheus.fcardoso@hotmail.com>
2024-08-25 17:48:04 -0300

    rebasing

---
+++ src/test/java/com/phoenix/pi/sales_platform/SalesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.pi.sales_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
