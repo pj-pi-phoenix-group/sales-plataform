25ce295acd324797fc65c1cab3d123abdb77a037
EduardoPH <dudu19424irma@gmail.com>
2024-09-12 11:13:56 -0300

    [feature/01] - Created ProductController

---
+++ src/main/java/com/phoenix/pi/sales_platform/controller/ProductController.java
@@ -0,0 +1,8 @@
+package com.phoenix.pi.sales_platform.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/product")
+public class ProductController {
+}
