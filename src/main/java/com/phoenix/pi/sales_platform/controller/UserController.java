e5f8b63c697bfac97fcbe5f1e6ce4ecdd828cd6c
MattFC <matheus.fcardoso@hotmail.com>
2024-09-18 10:43:06 -0300

    [02] added swagger doc, changed postgres to h2, add some attributes to user entity

--- src/main/java/com/phoenix/pi/sales_platform/controller/UserController.java
+++ src/main/java/com/phoenix/pi/sales_platform/controller/UserController.java
@@ -1,7 +1,15 @@
 package com.phoenix.pi.sales_platform.controller;
 
+import com.phoenix.pi.sales_platform.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("/v1/users")
 public class UserController {
+
+    @GetMapping
+    public void test (User user){}
 }
--- src/main/java/com/phoenix/pi/sales_platform/model/User.java
+++ src/main/java/com/phoenix/pi/sales_platform/model/User.java
@@ -1,7 +1,19 @@
 package com.phoenix.pi.sales_platform.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-//@Entity
+import java.util.UUID;
+
+@Entity
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String number;
+    private String email;
+    private String password;
 }
