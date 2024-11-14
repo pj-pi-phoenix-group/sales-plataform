31db66bb51d6dc067958cf9eb3efb2edc0ab9767
MattFC <matheus.fcardoso@hotmail.com>
2024-11-14 11:23:53 -0300

    [feat] add crud for users

--- src/main/java/com/phoenix/pi/sales_platform/exception/UserException.java
+++ src/main/java/com/phoenix/pi/sales_platform/exception/UserException.java
@@ -1,4 +1,6 @@
 package com.phoenix.pi.sales_platform.exception;
 
-public class UserException {
+public class UserException extends RuntimeException {
+    public UserException(String s) {
+        super(s);    }
 }
--- src/main/java/com/phoenix/pi/sales_platform/repository/UserRepository.java
+++ src/main/java/com/phoenix/pi/sales_platform/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.phoenix.pi.sales_platform.repository;
 
 import com.phoenix.pi.sales_platform.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT * FROM users u WHERE u.email = :email", nativeQuery = true)
+    Optional<User> findByEmail(String email);
 }
